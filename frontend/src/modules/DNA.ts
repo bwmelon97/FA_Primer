da427b3d6a74485345d402df678233ad8a95df42
Soogeun Park <bwmelon97@naver.com>
2021-03-17 14:11:53 +0000

    f/DNA: 상보결합 결과물에서 스페이스바 등의 다른 문자열이 사라지는 문제 해결

--- frontend/src/modules/DNA.ts
+++ frontend/src/modules/DNA.ts
@@ -81,6 +81,7 @@ const createComplementaryString = (str: string) => {
                 break;
 
             default:
+                complementaryStringArr.push(c);
                 break;
         }
     }
